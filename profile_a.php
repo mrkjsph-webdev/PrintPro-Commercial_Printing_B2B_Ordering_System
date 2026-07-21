a267176f74f30792ba5eb1d3e410ff5225260c0a
mrkjsph. <markjoseph.lopena741@gmail.com>
2026-07-21 21:38:17 +0800

    Add files via upload

---
+++ profile_a.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PROFILE A</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <style>
+            a {
+                text-decoration: none;
+                color: rgb(0, 0, 0);
+            }
+            .text {
+                color: rgb(69, 69, 182);
+                font-weight: bold;
+            }
+            hr {
+                border: 0;
+                height: 1px;
+                background-color: rgb(69, 69, 182);
+                color: rgb(69, 69, 182);
+            }
+        </style>
+</head>
+
+<body>
+    <div class="container mt-4">
+        <div class="card mx-auto shadow-sm" style="max-width: 1000px;">
+            <div class="card-body px-4 py-4">
+                <div class="row mb-4">
+                    <div class="col-12">
+                        <h2 class="card-title text">MARK JOSEPH B. LOPENA</h2>
+                        <h4 class="card-subtitle mb-2">Aspiring Front-End Developer</h4>
+                        <div class="d-flex flex-wrap gap-2">
+                            <div>Cavite, Philippines</div>
+                            <div>|</div>
+                            <div>+639265590713</div>
+                            <div>|</div>
+                            <div>markjoseph.lopena741@gmail.com</div>
+                            <div>|</div>
+                            <div><a href="https://github.com/mrkjsph-webdev" target="_blank">github.com/mrkjsph-webdev</a></div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row gy-3">
+                    <div class="col-12">
+                        <h6 class="card-title text">CAREER OBJECTIVE</h6><hr>
+                        <p>Information Technology undergraduate with a strong foundation in web development, UI/UX design, and graphic design. Experienced in developing academic and personal projects using modern technologies. Seeking an entry-level web developer position where I can apply my technical skills, contribute to real-world projects, and continue growing as an IT professional.</p>
+                    </div>
+                    <div class="col-12">
+                        <h6 class="card-title text">PROJECTS</h6><hr>
+                        <div class="mb-3">
+                            <p class="mb-1"><b>PrintPro - Commercial B2B Ordering System</b></p>
+                            <p class="mb-0"><i>UI/UX Designer</i></p>
+                            <ul>
+                                <li>Designed responsive user interfaces and high-fidelty layouts using Figma, ensuring a visually consistent experience across application screens.</li>
+                                <li>Created wireframes, mockups, and interactive prototypes to visualize and test design concepts.</li>
+                                <li>Collaborated with team members to refine design interfaces based on feedback and usability testing.</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p class="mb-1"><b>MOTIVO - Fitness Goal App</b></p>
+                            <p class="mb-0"><i>Front-End Developer</i></p>
+                            <ul>
+                                <li>Developed responsive Android user interface using Java, XML, and Android Studio.</li>
+                                <li>Implemented user interface designs into functional application screens while maintaining code quality and performance.</li>
+                                <li>Optimized application performance and provided smooth user experience.</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <h6 class="card-title text">SKILLS</h6><hr>
+                        <ul>
+                            <li>HTML/CSS/JavaScript</li>
+                            <li>React JS</li>
+                            <li>Bootstrap</li>
+                            <li>PHP</li>
+                            <li>MySQL</li>
+                            <li>Figma</li>
+                            <li>UI/UX Design</li>
+                        </ul>
+                    </div>
+                     <div class="col-12">
+                        <h6 class="card-title text">EDUCATION</h6><hr>
+                        <div class="row align-items-center">
+                            <div class="col-auto pe-0">
+                                <p class="mb-1"><b>Bachelor of Science in Information Technology with specialization in Mobile and Web Applications</b></p>
+                            </div>
+                            <div class="col text-end">
+                                <p class="mb-1 text-body-secondary">2024–2028</p>
+                            </div>
+                        </div>
+                        <p class="mb-0"><i>National University Dasmariñas</i></p>
+                    </div>
+                    <div class="col-12">
+                        <h6 class="card-title text">CERTIFICATIONS</h6><hr>
+                        <ul>
+                            <li>TOEIC Listening & Reading - Score: 790/990 (2026)</li>
+                        </ul>
+                    </div>
+                    <div class="col-12">
+                        <h6 class="card-title text">LEADERSHIP & EXTRACURRICULAR ACTIVITIES</h6><hr>
+                        <ul>
+                            <li>Member, NUD Computer Society</li>
+                            <li>Member, Junior Philippine Computer Society - NUD Chapter</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
